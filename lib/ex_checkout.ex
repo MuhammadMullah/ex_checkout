ddbb8243aff7a356465175104bf39057f51e62e8
AbuAsmahan <mohamed.ali@wmcontractor.com>
2024-11-09 19:52:20 +0300

    ready set code.....

---
+++ lib/ex_checkout.ex
@@ -0,0 +1,18 @@
+defmodule ExCheckout do
+  @moduledoc """
+  Documentation for `ExCheckout`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCheckout.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_checkout_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCheckoutTest do
+  use ExUnit.Case
+  doctest ExCheckout
+
+  test "greets the world" do
+    assert ExCheckout.hello() == :world
+  end
+end
